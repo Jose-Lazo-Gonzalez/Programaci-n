1200fc3ce537a5531aafd8c9bf4b5660f2a4ded4
José Lazo González <jlazgon004@g.educaand.es>
2025-10-08 08:13:40 +0200

    subo

---
+++ ej4.py
@@ -0,0 +1,6 @@
+suma = 0
+numero=1
+while numero != 0:
+    numero = int(input("Ingresa un número (0 para terminar): "))
+    suma += numero
+print (suma)
--- while.py
+++ while.py
@@ -1,5 +1,4 @@
 a = input("Has pedido el bono cultural?")
-a.upper(a)
 while a!="SI":
     print("pidelo")
     a = input("Has pedido el bono cultural?")
