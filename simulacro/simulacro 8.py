7f948a5db5010d3151e9ed27382984ad632dd196
Jose Lazo Gonzalez <jlazgon004@g.educaand.es>
2025-11-06 19:43:19 +0100

    subo

---
+++ simulacro/simulacro 8.py
@@ -0,0 +1,35 @@
+impares=[]
+contaimpar=0
+num1=int(input("introduce un numerin:"))
+num2=int(input("introduce otro numerin"))
+while num1>=num2:
+    num1=int(input("introduce un numerin:"))
+    num2=int(input("introduce otro numerin"))
+if num1<num2:
+    a=input("el rango es abierto o es cerrado")
+    if a=="cerrado":
+        while num1 !=0 and num2!=0:
+            for i in range(num1,num2+1):
+                if i%2==1:
+                    impares.append(i)
+                    contaimpar+=1
+                    
+            print("=========================================================")
+            print(f"Impares que existen entre {num1} y el {num2}:{impares} ")
+            print(f"En total existen {contaimpar} números impares en el rango.")
+            print("=========================================================")
+            num1=int(input("introduce un numerin:"))
+            num2=int(input("introduce otro numerin"))
+    if a=="abierto":
+        while num1 !=0 and num2!=0:
+            for i in range(num1+1,num2):
+                if i%2==1:
+                    impares.append(i)
+                    contaimpar+=1
+                    
+            print("=========================================================")
+            print(f"Impares que existen entre {num1} y el {num2}:{impares} ")
+            print(f"En total existen {contaimpar} números impares en el rango.")
+            print("=========================================================")
+            num1=int(input("introduce un numerin:"))
+            num2=int(input("introduce otro numerin"))
